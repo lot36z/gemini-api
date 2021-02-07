ecbfb07fdbb68744c96b85afefdd5acf5390a6da
lot36z <knocker.lot36z@gmail.com>
2021-02-07 15:44:46 +0900

    init

---
+++ src/main/java/com/example/geminiapi/GeminiApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.geminiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeminiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeminiApiApplication.class, args);
+	}
+
+}
